00adad5bde0df3d4067fee6e3927886ecbea5670
Reuben Richardson <reuben_richardson@outlook.com>
2025-05-18 22:25:14 +1000

    Initial commit. EEGNet working on PhysioNet data.

---
+++ data_processing.py
@@ -0,0 +1,140 @@
+from mne.datasets import eegbci
+from mne.io import concatenate_raws, read_raw_edf
+import mne
+import matplotlib.pyplot as plt
+import numpy as np
+
+DATA_DIR = "/home/reuben/Documents/eeg-data/"
+
+def process_physionet(subject, name, task, verbose=False, visualise=False):
+    """
+    Task 1 (open and close left or right fist)
+    Task 2 (imagine opening and closing left or right fist)
+    Task 3 (open and close both fists or both feet)
+    Task 4 (imagine opening and closing both fists or both feet)
+    """
+    runs = [task + 2, task + 6, task + 10]
+
+    #Get data and locate in to given path
+    files = eegbci.load_data(subject, runs, path=DATA_DIR)
+    #Read raw data files where each file contains a run
+    raws = [read_raw_edf(f, preload=True) for f in files]
+
+    # -------------------------------------
+    if verbose:
+        print("FILES:")
+        for f in files:
+            print(" - ", f)
+
+        for raw in raws:
+            print("RAW", raw.filenames)
+            print(" - shape:", raw.get_data().shape)
+    # -------------------------------------
+
+    # Combine all loaded runs
+    raw_obj = concatenate_raws(raws)
+    raw_data = raw_obj.get_data()
+
+    map = {
+    'Fc5.': 'FC5', 'Fc3.': 'FC3', 'Fc1.': 'FC1', 'Fcz.': 'FCz', 'Fc2.': 'FC2',
+    'Fc4.': 'FC4', 'Fc6.': 'FC6', 'C5..': 'C5', 'C3..': 'C3', 'C1..': 'C1',
+    'Cz..': 'Cz', 'C2..': 'C2', 'C4..': 'C4', 'C6..': 'C6', 'Cp5.': 'CP5',
+    'Cp3.': 'CP3', 'Cp1.': 'CP1', 'Cpz.': 'CPz', 'Cp2.': 'CP2', 'Cp4.': 'CP4',
+    'Cp6.': 'CP6', 'Fp1.': 'Fp1', 'Fpz.': 'Fpz', 'Fp2.': 'Fp2', 'Af7.': 'AF7',
+    'Af3.': 'AF3', 'Afz.': 'AFz', 'Af4.': 'AF4', 'Af8.': 'AF8', 'F7..': 'F7',
+    'F5..': 'F5', 'F3..': 'F3', 'F1..': 'F1', 'Fz..': 'Fz', 'F2..': 'F2',
+    'F4..': 'F4', 'F6..': 'F6', 'F8..': 'F8', 'Ft7.': 'FT7', 'Ft8.': 'FT8',
+    'T7..': 'T7', 'T8..': 'T8', 'T9..': 'T9', 'T10.': 'T10', 'Tp7.': 'TP7',
+    'Tp8.': 'TP8', 'P7..': 'P7', 'P5..': 'P5', 'P3..': 'P3', 'P1..': 'P1',
+    'Pz..': 'Pz', 'P2..': 'P2', 'P4..': 'P4', 'P6..': 'P6', 'P8..': 'P8',
+    'Po7.': 'PO7', 'Po3.': 'PO3', 'Poz.': 'POz', 'Po4.': 'PO4', 'Po8.': 'PO8',
+    'O1..': 'O1', 'Oz..': 'Oz', 'O2..': 'O2', 'Iz..': 'Iz'}
+
+    raw_obj.rename_channels(map)
+
+    raw_obj.set_montage("standard_1005")
+
+    # Extract events from raw data
+    events, event_ids = mne.events_from_annotations(raw_obj, event_id='auto')
+
+    # Create EPOCHS
+    tmin, tmax = -1, 4  # define epochs around events (in s)
+    epochs = mne.Epochs(raw_obj, events, event_ids, tmin, tmax, baseline=None, preload=True)
+
+    # Save Epochs
+    epochs.save(DATA_DIR + '/physionet-fifs/' + name + '-epo.fif', overwrite=True)
+    
+    # -------------------------------------
+    if verbose:
+        # events = mne.events_from_annotations(raw_obj)
+        print("events[1]:", events[1])
+        print("Number of channels: ", str(len(raw_data)))
+        print("Number of samples: ", str(len(raw_data[0])))
+        print("event_ids:", event_ids)
+        print("events.shape:", events.shape)
+        print("events:", events)
+
+        # Print Epoch info
+        data = epochs._data
+        n_events = len(data) # or len(epochs.events)
+        print("Number of events: " + str(n_events)) 
+        n_channels = len(data[0,:]) # or len(epochs.ch_names)
+        print("Number of channels: " + str(n_channels))
+        n_times = len(data[0,0,:]) # or len(epochs.times)
+        print("Number of time instances: " + str(n_times))
+        print('Start time before the event' , epochs.tmin)
+        print('Stop time after the event' , epochs.tmax)
+
+        rest_epochs = epochs['T0']
+        left_fist_epochs = epochs['T1']
+        right_fist_epochs = epochs['T2']
+        print("Number of T0 (Rest) events (epochs):", len(rest_epochs._data))
+        print("Number of T1 events (epochs):", len(left_fist_epochs._data))
+        print("Number of T2 events (epochs):", len(right_fist_epochs._data))
+
+
+    if visualise:
+        # Example plot of raw data
+        plt.plot(raw_data[0][:4999])
+        plt.title("Raw EEG, electrode 0, samples 0-4999")
+        plt.show()
+
+        # Plot electrode positions
+        fig = raw_obj.plot_sensors(show_names=True)
+        plt.show()
+
+        # Power plot and MNE raw plot
+        raw_obj.compute_psd(fmax=50).plot(picks="data", exclude="bads", amplitude=False)
+        raw_obj.plot(duration=5, n_channels=30);
+        plt.show()
+
+        # Plot epochs
+        epochs.plot(scalings='auto')
+
+        plt.plot(data[:,0,:].T)
+        plt.title("Exemplar single-trial epoched data, for electrode 0")
+        plt.show()
+
+        epochs['T0'].average().plot()
+        epochs['T1'].average().plot()
+        epochs['T2'].average().plot()
+
+        # Plot topomaps
+        times = np.arange(0, 1, 0.1)
+        epochs.average().plot_topomap(times, ch_type='eeg')
+        epochs["T0"].average().plot_topomap(times, ch_type='eeg')
+        epochs["T1"].average().plot_topomap(times, ch_type='eeg')
+        epochs["T2"].average().plot_topomap(times, ch_type='eeg')
+
+        plt.show()
+
+    # -------------------------------------
+
+if __name__ == "__main__":
+    print("Running `data_processing.py` directly")
+    # process_physionet(3, "S3-MI-FF")#, verbose=True, visualise=True)
+
+    # for i in range(1, 25):
+    #     process_physionet(i, "/task2/s" + str(i), 2)
+
+    
---
+++ train_EEGNet.py
@@ -0,0 +1,130 @@
+import mne
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Torch
+import torch
+import torch.nn as nn
+from torchsummary import summary
+from torch.utils.data import TensorDataset
+
+from sklearn.model_selection import train_test_split
+
+import json
+
+import EEGNet
+
+DATA_DIR = "/home/reuben/Documents/eeg-data/"
+MODELS_DIR = "./models/"
+
+def load(load_path):
+    epochs = mne.read_epochs(load_path)
+    data = epochs.get_data(copy=True)
+    labels = epochs.events[:,-1]
+    return data, labels
+
+def train(name, load_path, epochs):
+    # Choosing Device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Loss Function
+    criterion = nn.CrossEntropyLoss()
+
+    data, labels = load(load_path)
+
+    # Normalizing Labels to [0, 1, 2]
+    y = labels - np.min(labels)
+
+    # Normalizing Input features: z-score(mean=0, std=1)
+    X = (data - np.mean(data)) / np.std(data)
+
+    # Spliting  Data: 90% for Train and 10% for Test
+    X_train, X_test_, y_train, y_test_ = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
+
+    # Converting to Tensor
+    X_train = torch.Tensor(X_train).unsqueeze(1).to(device)
+    X_test = torch.Tensor(X_test_).unsqueeze(1).to(device)
+    y_train = torch.LongTensor(y_train).to(device)
+    y_test = torch.LongTensor(y_test_).to(device)
+
+    # Creating Tensor Dataset
+    train_dataset = TensorDataset(X_train, y_train)
+    # test_dataset = TensorDataset(X_test, y_test)
+
+    # Printing the sizes
+    print("Size of X_train:", X_train.size())
+    print("Size of X_test:", X_test.size())
+    print("Size of y_train:", y_train.size())
+    print("Size of y_test:", y_test.size())
+
+    print(y_train)
+    print(y_test)
+
+    ## MODEL SUMMARY
+
+    # input_size = (1, 64, 961)
+    # eegnet_model = EEGNet.EEGNetModel().to(device)
+    # summary(eegnet_model, input_size)
+
+    chans = X_train.size()[2]
+    time_points = X_train.size()[3]
+
+    eegnet_model = EEGNet.EEGNetModel(chans=chans, time_points=time_points).to(device)
+
+    # Training Hyperparameters
+    EPOCHS = epochs
+    BATCH_SIZE = 64
+    LEARNING_RATE = 0.001
+    trainer = EEGNet.TrainModel()
+    trained_eegnet_model, train_info = trainer.train_model(eegnet_model, train_dataset, learning_rate=LEARNING_RATE,
+                                    batch_size=BATCH_SIZE, epochs=EPOCHS)
+    torch.save(trained_eegnet_model.state_dict(), MODELS_DIR + name + '.pth')
+    
+    train_metas = [train_info, X_test_.tolist(), y_test_.tolist(), chans, time_points]
+
+    with open(MODELS_DIR + name + ".json", 'w') as json_file1:
+        json.dump(train_metas, json_file1)
+
+def evaluate(name):
+    with open(MODELS_DIR + name + ".json", 'r') as json_file1:
+        train_metas_loaded = json.load(json_file1)
+
+    train_info, X_test_, y_test_, chans, time_points = train_metas_loaded
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    X_test = torch.Tensor(X_test_).unsqueeze(1).to(device)
+    y_test = torch.LongTensor(y_test_).to(device)
+
+    test_dataset = TensorDataset(X_test, y_test)
+
+    trained_eegnet_model = EEGNet.EEGNetModel(chans=chans, time_points=time_points).to(device)
+    trained_eegnet_model.load_state_dict(torch.load('eegnet_model.pth', map_location=torch.device('cpu')))
+    trained_eegnet_model.eval()
+    classes_list = ['rest', 'hands', 'feet']
+    eval_model = EEGNet.EvalModel(trained_eegnet_model, MODELS_DIR + name)
+    test_accuracy = eval_model.test_model(test_dataset)
+    eval_model.plot_confusion_matrix(test_dataset, classes_list)
+
+    plt.plot(train_info[0], train_info[1], label="Loss")
+    plt.title("Loss Curve")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.savefig(MODELS_DIR + name + '_loss_curve.png')
+    plt.show()
+
+    plt.plot(train_info[0], train_info[2], label="Accuracy")
+    plt.title("Accuracy Curve")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.savefig(MODELS_DIR + name + '_accuracy_curve.png')
+    plt.show()
+
+if __name__ == "__main__":
+    print("Running 'train_EEGNet.py' directly")
+    
+    for i in range(1, 25):
+        name = "task1_s" + str(i)
+        load_path = DATA_DIR + "/physionet-fifs/task1/s" + str(i) + "-epo.fif"
+        train(name, load_path, epochs=200)
+        # evaluate(name)
